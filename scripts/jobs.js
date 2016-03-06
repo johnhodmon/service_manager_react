36dd6f83e29d4da3b79f83adb2159304e549794f
John Hodmon <johnhodmon@gmail.com>
2016-03-06 17:37:37 +0000

    added router and links in navbar

--- scripts/jobs.js
+++ scripts/jobs.js
@@ -1,18 +1,44 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
 var jobs=require('../data/JobData.js').jobs;
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Link = ReactRouter.Link;
+var IndexRoute = ReactRouter.IndexRoute
+$(document).ready(function() {
+
+    var dynamic = $('.side-pane');
+    var static = $('.main-pane');
+
+    static.height(dynamic.height());
+
+});
+
+var App=React.createClass(
+    {
+        render:function()
+        {
+            return(
+                <div>
 
 
+                    {this.props.children}
+                </div>
+            );
+        }
+    }
+);
 
 var JobPage=React.createClass(
     {
         render:function()
             {
                 return(
-                   <div className="container">
+                   <div className="container-fluid">
                    <Navbar />
-                   <PageContent jobs={this.props.jobs} />
-                       </div>
+                   <PageContent  />
+                   </div>
 
 
                 );
@@ -21,20 +47,88 @@ var JobPage=React.createClass(
 
     }
 );
+var CustomerPage=React.createClass(
+    {
+        render:function()
+        {
+            return(
+                <div className="container-fluid">
+                    <Navbar />
+                    <h1>Customers</h1>
+                </div>
+
+
+            );
+
+        }
+
+    }
+);
+
+var ProductPage=React.createClass(
+    {
+        render:function()
+        {
+            return(
+                <div className="container-fluid">
+                    <Navbar />
+                    <h1>Products</h1>
+                </div>
+
+
+            );
+
+        }
+
+    }
+);
+
+var PartPage=React.createClass(
+    {
+        render:function()
+        {
+            return(
+                <div className="container-fluid">
+                    <Navbar />
+                    <h1>Parts</h1>
+                </div>
+
+
+            );
+
+        }
+
+    }
+);
+
 
 var Navbar=React.createClass({
         render: function ()
         {
             return(
-                <div className="row">
-                    <div role="navigation" className="col-md-12">
-                        <ul className="nav navbar-nav">
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Customers</a></li>
-                            <li><a href="#">Products</a></li>
-                            <li><a href="#">Stock Control</a></li>
+                <div className="row top-nav">
+                    <div className="col-md-2">
+                        <img src="/img/logo.jpg" />
+                        </div>
+                    <div className="col-md-10 top-nav-div">
+                        <ul className="nav  nav-tabs">
+                            <li className="active"> <Link to="/jobs">Jobs</Link></li>
+                            <li> <Link to="/customers">Customers</Link></li>
+                            <li>   <Link to="/products">Products</Link></li>
+                            <li>   <Link to="/parts">Stock Control</Link></li>
+                            <li role="presentation" className="dropdown">
+                                <a className="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                                    John Hodmon<span className="caret"></span>
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li>Logout</li>
+
+
+                                </ul>
+                            </li>
                         </ul>
-                    </div>
+                        </div>
+
                 </div>
 
 
@@ -49,11 +143,11 @@ var PageContent=React.createClass({
     {
         return(
             <div className="row">
-                <div className="col-md-4">
+                <div className="col-md-2 side-pane">
                     <SideBar jobs={this.props.jobs}/>
 
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-10 main-pane">
                     <MainPane/>
                 </div>
 
@@ -67,10 +161,14 @@ var PageContent=React.createClass({
 var SideBar=React.createClass({
     render:function(){
         return(
-            <div className="row">
+            <div>
+            <div className="row search-box-div">
             <Searchbox/>
-            <List jobs={this.props.jobs}/>
+                </div>
+                <div className="row">
+            <List jobs={["Test","Test"]}/>
             </div>
+                </div>
 
         );
     }
@@ -79,9 +177,9 @@ var SideBar=React.createClass({
 var MainPane=React.createClass({
     render:function(){
         return(
-            <div className="row">
+            <div>
                 <div className="col-md-3">
-
+                Customer details here
                 </div>
                 <div className="col-md-6">
                 Job Details Here
@@ -104,15 +202,16 @@ var Searchbox=React.createClass({
 
         return(
 
-                <div className="row" >
-                    <div className="col-md-6">
+                <div>
+                    <div className="row">
                     <input type="text"  placeholder="Search"/>
                      </div>
-                    <div className="col-md-6">
+                    <div className="row">
                     <select id="sort" >
+                        <option value="" disabled selected>Sort by: </option>
                         <option value="date">Date</option>
                         <option value="name">Customer</option>
-                        <option value="product">Customer</option>
+                        <option value="product">Status</option>
                     </select>
                         </div>
 
@@ -134,9 +233,11 @@ var List=React.createClass(
 
 
                 return(
-                    <div className="col-md-10">
-                        <ul className="phones">
+                    <div >
+                        <ul className="nav nav-pills nav-stacked side-nav" >
+
                             {jobsToDisplay}
+
 
                         </ul>
                     </div>
@@ -154,15 +255,17 @@ var SingleJob=React.createClass({
 
         return (
 
-            <li >
-                <p>
-                    <a href="#">{job.date}</a><br/>
-                     <a href="#">{job.customerProduct.product.manufacturer.name+" "+job.customerProduct.product.description}</a><br/>
-                    <a href="#">{job.customer.name}</a>  <br/>
+            <li role="presentation" >
+
+
+                {/* <a href="#"><h3>{job.date}</h3>
+                    <p>{job.customerProduct.product.manufacturer.name+" "+job.customerProduct.product.description.split(",")[0]}<br/>
+                    {job.customer.name}  <br/>
                     {job.customer.town}<br/>
+                        {job.status}<br/></p></a>*/}
 
 
-                </p>
+                Test
 
 
 
@@ -170,7 +273,18 @@ var SingleJob=React.createClass({
     }
 });
 
-ReactDOM.render(
-    <JobPage jobs={jobs} />,
+
+
+ReactDOM.render( (
+        <Router >
+            <Route path="/" component={App}>
+                <IndexRoute component={JobPage}/>
+                <Route path="jobs" component={JobPage}  />
+                <Route path="customers" component={CustomerPage} />
+                <Route path="products" component={ProductPage} />
+                <Route path="Parts" component={PartPage} />
+            </Route>
+        </Router>
+    ),
     document.getElementById('mount-point')
 );
